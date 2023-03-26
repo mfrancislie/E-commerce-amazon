1b6ea55af003d286381c564d2bff7ae5861a361b
Francislie <mfrancislie@gmail.com>
2023-03-26 13:15:43 -0700

    added implement add to cart action

---
+++ frontend/src/reducers/cartReducer.js
@@ -0,0 +1,24 @@
+import { CART_ADD_ITEM } from '../constants/productsConstants';
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      // item as an action
+      const item = action.payload;
+      const existItem = state.cartItems.find((x) => x.product === item.product);
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((x) =>
+            x.id === existItem.product ? item : x
+          ),
+        };
+      } else {
+        // this code concatenate card items with the new item. So suppose that we have two items in cart items and we have a new one.
+        // The result of this expression is going to be three items.
+        return { ...state, cartItems: [...state.cartItems, item] };
+      }
+    default:
+      return state;
+  }
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,15 +5,24 @@ import {
   legacy_createStore,
 } from 'redux';
 import thunk from 'redux-thunk';
+import { cartReducer } from './reducers/cartReducer';
 import {
   productListReducers,
   detailsProductReducers,
 } from './reducers/productReducers';
-const initialState = {};
+const initialState = {
+  cart: {
+    // I need to pass it to Jason that pass to convert it to array.
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
+  },
+};
 
 const reducer = combineReducers({
   productList: productListReducers,
   productDetails: detailsProductReducers,
+  cart: cartReducer,
 });
 const composeEnhacer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = legacy_createStore(
