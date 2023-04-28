a2cc84946c8b3b533531a89ce5fad74590fc6abb
Francislie <mfrancislie@gmail.com>
2023-04-28 04:02:03 -0700

    to implement delete in userlist

--- frontend/src/screens/OrderListScreen.js
+++ frontend/src/screens/OrderListScreen.js
@@ -57,36 +57,42 @@ const OrderListScreen = () => {
             </tr>
           </thead>
           <tbody>
-            {orders.map((order) => (
-              <tr key={order._id}>
-                <td>{order._id}</td>
-                <td>{order.user.name}</td>
-                <td>{order.createdAt?.substring(0, 10)}</td>
-                <td>{order.totalPrice.toFixed(2)}</td>
-                <td>{order.isPaid ? order.paidAt?.substring(0, 10) : 'No'}</td>
-                <td>
-                  {order.isDelivered
-                    ? order.deliveredAt?.substring(0, 10)
-                    : 'No'}
-                </td>
-                <td>
-                  <button
-                    type="button"
-                    className="small"
-                    onClick={() => navigate(`/order/${order._id}`)}
-                  >
-                    Details
-                  </button>
-                  <button
-                    type="button"
-                    className="small"
-                    onClick={() => deleteHandler(order)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
+            {orders.length === 0 ? (
+              <MessageBox variant="danger block">Empty Orders</MessageBox>
+            ) : (
+              orders.map((order) => (
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>{order.user.name}</td>
+                  <td>{order.createdAt?.substring(0, 10)}</td>
+                  <td>{order.totalPrice.toFixed(2)}</td>
+                  <td>
+                    {order.isPaid ? order.paidAt?.substring(0, 10) : 'No'}
+                  </td>
+                  <td>
+                    {order.isDelivered
+                      ? order.deliveredAt?.substring(0, 10)
+                      : 'No'}
+                  </td>
+                  <td>
+                    <button
+                      type="button"
+                      className="small"
+                      onClick={() => navigate(`/order/${order._id}`)}
+                    >
+                      Details
+                    </button>
+                    <button
+                      type="button"
+                      className="small"
+                      onClick={() => deleteHandler(order)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))
+            )}
           </tbody>
         </table>
       )}
