88b64a944ea7c65051aae8c3dc3f4060e76dc1ec
Francislie <mfrancislie@gmail.com>
2023-05-06 00:15:01 -0700

    force item from one seller

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -12,7 +12,7 @@ const CartScreen = () => {
 
   const navigate = useNavigate();
   const cart = useSelector((state) => state.cart);
-  const { cartItems } = cart;
+  const { cartItems, error } = cart;
   const dispatch = useDispatch();
   useEffect(() => {
     if (productId) {
@@ -33,6 +33,7 @@ const CartScreen = () => {
     <div className="row top">
       <div className="col-2">
         <h1>Shopping Cart</h1>
+        {error && <MessageBox variant="danger">{error}</MessageBox>}
         {cartItems.length === 0 ? (
           <MessageBox>
             Cart is empty. <Link to="/">Go Shopping</Link>
